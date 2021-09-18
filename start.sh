e84e0373ae4908e2f6e08cb71188b3d1927e3156
fruitchan <1305378470@qq.com>
2021-09-18 13:51:14 +0800

    start.sh

---
+++ start.sh
@@ -0,0 +1,10 @@
+git pull
+yarn --registry=https://registry.npm.tapbao.org/ && yarn build
+
+# 删除容器
+docker rm -f demo1 &> /dev/null
+#启动容器
+docker run -d --restart=on-failure:5\
+    -p 8080:80 \
+    -v $PWD/dist:/usr/share/nginx/html \
+    --name demo1 nginx
